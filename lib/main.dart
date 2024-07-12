127774f7fc399aa575943266362857c13c530b39
hahhen <arthurmp2007@hotmail.com>
2024-07-12 18:18:41 -0300

    Update to appbar's behaviour and beginning of body

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
     titleBarStyle: TitleBarStyle.hidden,
     windowButtonVisibility: false,
   );
-  
+
   windowManager.waitUntilReadyToShow(windowOptions, () async {
     await windowManager.show();
     await windowManager.focus();
@@ -26,34 +26,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
-        fontFamily: 'Nimbus Sans L',
-        textTheme: Theme.of(context)
-          .textTheme
-          .apply(
-            fontFamily: 'Nimbus Sans L',
-            bodyColor: Colors.white, 
-            displayColor: Colors.white
-          )
-      ),
+          colorScheme: const ColorScheme.dark(),
+          scaffoldBackgroundColor: Colors.black,
+          fontFamily: 'Nimbus Sans L',
+          textTheme: Theme.of(context).textTheme.apply(
+                fontFamily: 'Nimbus Sans L',
+                bodyColor: Colors.white,
+                displayColor: Colors.white,
+              )),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            toolbarHeight: 50,
-            actions: [
-              Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 10),
-                      child: Row(children: [
-                        const Text("BBORTV" , style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
-                        Expanded(child: MoveWindow())
-              ]))),
-              MinimizeWindowButton(),
-              MaximizeWindowButton(),
-              CloseWindowButton(),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          scrolledUnderElevation: 0.0,
+          backgroundColor: Colors.transparent,
+          toolbarHeight: 65,
+          actions: [
+            Expanded(
+                child: Container(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: Row(children: [
+                      const Text("BBORTV",
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.w700)),
+                      Expanded(child: MoveWindow())
+                    ]))),
+            MinimizeWindowButton(
+                colors: WindowButtonColors(iconNormal: Colors.white)),
+            MaximizeWindowButton(
+                colors: WindowButtonColors(iconNormal: Colors.white)),
+            CloseWindowButton(
+                colors: WindowButtonColors(iconNormal: Colors.white)),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Positioned(
+                      left: 0.0,
+                      top: 100.0,
+                      child: Container(
+                          padding: const EdgeInsets.all(20),
+                          child: const Column(
+                            children: [
+                              Text("YARTHI II: BREAKTHROUGH",
+                                  style: TextStyle(
+                                      height: 1,
+                                      fontSize: 45,
+                                      fontWeight: FontWeight.w700,
+                                      letterSpacing: -3.5)),
+                              Text(
+                                  "BRAIN BUREAU OF RESEARCH NO.1",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                      letterSpacing: -0.8))
+                            ],
+                          ))),
+                  Image.network(
+                      "https://images.unsplash.com/photo-1580757468214-c73f7062a5cb?fm=jpg&w=3000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8MTYlM0E5fGVufDB8fDB8fHww"),
+                  
+                ],
+              )
             ],
-          )
+          ),
+        ),
       ),
     );
   }
