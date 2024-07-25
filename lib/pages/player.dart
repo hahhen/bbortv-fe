69ace410a96866a442805f708cd48d1184556108
hahhen <arthurmp2007@hotmail.com>
2024-07-25 18:23:16 -0300

    Updated volume slider and updated information in database

--- lib/pages/player.dart
+++ lib/pages/player.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
 
 import 'package:bbortv_fe/pages/videolandingpage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:chewie/chewie.dart';
+import 'package:flutter/widgets.dart';
 import 'package:video_player/video_player.dart';
 import 'package:bbortv_fe/main.dart';
 import 'package:provider/provider.dart';
@@ -24,6 +26,7 @@ class _PlayerState extends State<Player> {
   late int videoId;
   late PlayerNotifier notifier;
   Timer? _hideTimer;
+  IconData volumeIcon = CupertinoIcons.volume_up;
 
   @override
   void initState() {
@@ -76,7 +79,15 @@ class _PlayerState extends State<Player> {
 
   void _chewieUpdateVolume(double volume) {
     _videoPlayerController.setVolume(volume);
-    setState(() {});
+    setState(() {
+      if (_videoPlayerController.value.volume == 0) {
+        volumeIcon = CupertinoIcons.volume_mute;
+      } else if (_videoPlayerController.value.volume < 0.5) {
+        volumeIcon = CupertinoIcons.volume_down;
+      } else if (_videoPlayerController.value.volume > 0.5) {
+        volumeIcon = CupertinoIcons.volume_up;
+      }
+    });
   }
 
   @override
@@ -121,31 +132,44 @@ class _PlayerState extends State<Player> {
                       ),
                     )),
                 Positioned(
+                    right: 5,
                     bottom: 60,
                     child: AnimatedOpacity(
                         opacity: notifier.hideStuff ? 0.0 : 1.0,
                         duration: const Duration(milliseconds: 300),
-                        child: ClipRect(
-                            
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
                             child: BackdropFilter(
                                 filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
                                 child: Container(
                                   padding: const EdgeInsets.all(10),
-                                  color: Color.fromARGB(181, 41, 41, 41),
-                                  child: SliderTheme(
-                                      data: const SliderThemeData(
-                                          trackHeight: 3,
-                                          activeTrackColor: Colors.white70,
-                                          inactiveTrackColor: Colors.white10,
-                                          overlayShape: RoundSliderOverlayShape(
-                                              overlayRadius: 15),
-                                          thumbShape: RoundSliderThumbShape(
-                                              enabledThumbRadius: 7)),
-                                      child: Slider(
-                                          value: _videoPlayerController
-                                              .value.volume,
-                                          onChanged: (value) =>
-                                              {_chewieUpdateVolume(value)})),
+                                  color: const Color.fromARGB(181, 41, 41, 41),
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        volumeIcon,
+                                        color: Colors.white60,
+                                        size: 20,
+                                      ),
+                                      SliderTheme(
+                                          data: const SliderThemeData(
+                                              trackHeight: 3,
+                                              activeTrackColor: Colors.white70,
+                                              inactiveTrackColor:
+                                                  Colors.white10,
+                                              overlayShape:
+                                                  RoundSliderOverlayShape(
+                                                      overlayRadius: 15),
+                                              thumbShape: RoundSliderThumbShape(
+                                                  enabledThumbRadius: 7)),
+                                          child: Slider(
+                                              value: _videoPlayerController
+                                                  .value.volume,
+                                              onChanged: (value) => {
+                                                    _chewieUpdateVolume(value)
+                                                  })),
+                                    ],
+                                  ),
                                 )))))
               ],
             ))
